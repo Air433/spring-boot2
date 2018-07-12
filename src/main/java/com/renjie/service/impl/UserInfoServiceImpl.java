a47a1d232b85af5f6bbc78e271b84587bf35a443
Air <398225229@qq.com>
2018-07-12 18:10:58 +0800

    更改查询UserBiz

--- src/main/java/com/renjie/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/renjie/service/impl/UserInfoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.renjie.service.impl;
 
 //import com.renjie.entity.SysPermission;
+import com.renjie.domain.bizs.RoleBiz;
+import com.renjie.domain.bizs.UserBiz;
+import com.renjie.entity.Authority;
 import com.renjie.entity.Role;
 import com.renjie.entity.UserInfo;
 import com.renjie.service.IUserInfoService;
@@ -22,6 +25,8 @@ public class UserInfoServiceImpl implements IUserInfoService {
         userInfo.setPassword("2093ff5efdd13a3156944657d8b4c131");
         List<Role> roleList = new ArrayList<Role>();
         Role role  = new Role();
+        role.setRoleKey("admin");
+
 //        sysRole.setRole("admin");
 //
 //        List<SysPermission> permissions = new ArrayList<SysPermission>();
@@ -33,5 +38,17 @@ public class UserInfoServiceImpl implements IUserInfoService {
         roleList.add(role);
         userInfo.setRoleList(roleList);
         return userInfo;
+    }
+    public UserBiz findByUsername(){
+        UserBiz userBiz = new UserBiz();
+        userBiz.setUserName("air");
+        userBiz.setPassword("2093ff5efdd13a3156944657d8b4c131");
+        List<Role> roleList = new ArrayList<Role>();
+        Role role  = new Role();
+        role.setRoleKey("admin");
+        RoleBiz roleBiz = new RoleBiz();
+        roleBiz.setRoleKey("admin");
+        Authority authority = new Authority();
+        authority.setMenuCode();
     }
 }
